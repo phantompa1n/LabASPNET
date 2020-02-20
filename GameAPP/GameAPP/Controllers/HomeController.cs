ac45175f843b76c1127a1b0df9d97e984867ca15
Sergey Desktop <isergei151@gmail.com>
2020-02-21 00:00:00 +0300

    Добавили генерацию поля

--- GameAPP/GameAPP/Controllers/HomeController.cs
+++ GameAPP/GameAPP/Controllers/HomeController.cs
@@ -56,12 +56,45 @@ namespace GameAPP.Controllers
         }
 
 
-        public static int getRandomPictures(int size)
+        public static int[] getRandomPictures(int size)
         {
             //Thread.Sleep(100);
-            int time = Convert.ToInt32(DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString());
-            Random rnd = new Random(time);
-            return rnd.Next(2, size * size);
+            int[] pole = new int[size * size];
+            pole[0] = 1;
+            //int time = Convert.ToInt32(DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString());
+            Random rnd = new Random();//(time);
+            for (int i = 1; i < size * size; i++)
+            {
+                int th = rnd.Next(2, size * size + 1);
+                if (NumberOnPole(pole, th))
+                {
+                    i--;
+                    continue;
+                }
+                pole[i] = th;
+            }
+            return pole;
+        }
+        static bool NumberOnPole(int[] pole, int th)
+        {
+            foreach (int p in pole)
+            {
+                if (p == th)
+                    return true;
+            }
+            return false;
+        }
+        public static bool WinPole(int[] pole)
+        {
+            int count = 1;
+            for (int i = 0; i < pole.Length - 1; i++)
+            {
+                if (pole[i] > pole[i + 1])
+                    count++;
+            }
+            if (count % 2 == 0)
+                return true;
+            return false;
         }
         public static int getRandomCars()
         {
